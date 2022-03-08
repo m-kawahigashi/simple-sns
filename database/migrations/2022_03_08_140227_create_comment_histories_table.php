af422c33429312f42042454937ab960c81f1198a
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-08 23:16:49 +0900

    commenthistoriesテーブルのマイグレーションファイルとモデルを作成

---
+++ database/migrations/2022_03_08_140227_create_comment_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_histories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_histories');
+    }
+}
