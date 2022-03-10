78ad2c2412161fefcf4553b933555562c8627c5a
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-10 22:48:30 +0900

    Sass、TypeScriptをビルドするように設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,16 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// TypeScriptとSassを使用できるように記述を変更
+ mix.ts('resources/ts/index.tsx', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .react()
+//     .sass('resources/sass/app.scss', 'public/css');
