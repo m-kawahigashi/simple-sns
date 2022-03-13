ede7142f30e321e620cdf7417c69fe2a1ba7f947
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-13 15:50:41 +0900

    Material-ui動作確認

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// 試しに
+import { Button } from '@material-ui/core';
+
 const App: React.FC = () => {
     return (
         <div>
             <p>こんにちは</p>
+            {/* Material-uiが使用できるか確認 */}
+            <Button color="primary">Material-ui!</Button>
         </div>
     )
 }
