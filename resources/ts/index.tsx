5265c9847fab6548034d9857071b68c23e27722e
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-14 22:06:57 +0900

    不要な記載を削除

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-// 試しに
-// import { Button } from '@material-ui/core';
-
-// const App: React.FC = () => {
-//     return (
-//         <div>
-//             <>
-//                 <p>こんにちは</p>
-//                 <p>できた</p>
-//                 {/* Material-uiが使用できるか確認 */}
-//                 <Button color="primary">Material-ui!</Button>
-//             </>
-//         </div>
-//     )
-// }
-
 if (document.getElementById('app')) {
     ReactDOM.render(<App />, document.getElementById('app'));
 }
