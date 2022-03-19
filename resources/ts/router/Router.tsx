9b23e42e81d79bb7dfbb03508ae9e3ad8ce5116a
m-kawahigashi <masanori0624ds16@yahoo.co.jp>
2022-03-19 18:24:39 +0900

    ログイン画面のルート設定を追加

--- resources/ts/router/Router.tsx
+++ resources/ts/router/Router.tsx
@@ -1,11 +1,15 @@
 import React, { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "../components/pages/Home";
+import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
 
 export const Router : VFC = memo(() => {
     return (
         <Switch>
+            <Route exact path="/">
+                <Login />
+            </Route>
             <Route path="/home">
                 <Home />
             </Route>
